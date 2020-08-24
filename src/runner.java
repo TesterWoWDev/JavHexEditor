6f6edcb97ad9501bfb170b55958d49c7a505038e
Connor <connorkilbourne@gmail.com>
2020-08-24 13:50:15 -0400

    introduced direct byte addition rather than only String insert/replace

--- src/runner.java
+++ src/runner.java
@@ -17,15 +17,30 @@ public class runner {
     public static void proofOfConcept(String arg1) throws IOException {
         readInFile(arg1);
         //either works
-        //String hex = Integer.toHexString(304);
-        String hex = "193";//dont include the 0x or the h ex.0x193h -> 193
+        String hex = Integer.toHexString(304);
+        //String hex = "193";//dont include the 0x or the h ex.0x193h -> 193
         printAtAddress(hex, 5);
-        replaceAtAddress(hex, "trash");
+        replaceStringAtAddress(hex, "trash");
         printAtAddress(hex, 5);
-        insertAtEndOfFile("trash");
         String value = getAtAddress(hex,5);
         System.out.println(value);
+        replaceBytesAtAddress(hex,"885533");
+        printAtAddress(hex, 5);
+        insertStringAtEndOfFile("trash");
+        insertBytesAtEndOfFile("772129");//no spaces for the bytes
+        printAtAddress(Integer.toHexString(fileContent.length-10), 10);
         saveFile();
+    }
+
+    public static String returnString(String input){
+        StringBuilder result = new StringBuilder();
+        char[] charArray = input.toCharArray();
+        for(int i = 0; i < charArray.length; i=i+2) {
+            String st = ""+charArray[i]+""+charArray[i+1];
+            char ch = (char)Integer.parseInt(st, 16);
+            result.append(ch);
+        }
+        return result.toString();
     }
 
     public static void readInFile(String name) throws IOException {
@@ -33,14 +48,22 @@ public class runner {
         fileContent = Files.readAllBytes(Paths.get(name));
     }
 
-    public static void replaceAtAddress(String address,String replacement){
+    public static void replaceBytesAtAddress(String address,String replacement){
+        replaceStringAtAddress(address,returnString(replacement));
+    }
+
+    public static void insertBytesAtEndOfFile(String replacement){
+       insertStringAtEndOfFile(returnString(replacement));
+    }
+    
+    public static void replaceStringAtAddress(String address,String replacement){
         byte[] replace = replacement.getBytes();
         int size = replace.length;
         int hex = Integer.parseInt(address,16);
         System.arraycopy(replace, 0, fileContent, hex, size);
     }
 
-    public static void insertAtEndOfFile(String replacement){
+    public static void insertStringAtEndOfFile(String replacement){
         byte[] replace = replacement.getBytes();
         int size = replace.length;
         byte[] holder = new byte[fileContent.length + size];
